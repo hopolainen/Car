c12af1e4a80ef7cbf52e18d09931d9b5035c23bb
hopolainen <niko.holopainen@student.hamk.fi>
2021-12-06 19:37:11 +0200

    välilyöntejä

--- Utils.java
+++ Utils.java
@@ -4,18 +4,20 @@ public class Utils {
 	public static void main (String[] args)
 	   {
 		   Scanner scanner = new Scanner(System.in);
+		   
 		   System.out.println("Width?");
 		   int length = scanner.nextInt();
+		   
 		   System.out.println("Length?");
 		   int width = scanner.nextInt();
 
 		   calcArea(width, length);
-
 	   }
 
 public static void calcArea(int width, int length){
 	
 	int area = (width*length);
+	
 	System.out.println("Area is " + (area));
 }
 }
